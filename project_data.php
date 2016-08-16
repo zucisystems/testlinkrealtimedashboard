d931ae5f6ac5457fa180a673998e25e044755e43
MALARVIZHI.NA <malarvizhi.n@zucisystems.com>
2016-08-16 08:50:49 +0530

    Update project_data.php

--- project_data.php
+++ project_data.php
@@ -30,7 +30,6 @@ function odd($var)
     return($var & '36');
 }
 $sql = "select name, id, node_type_id from nodes_hierarchy where node_type_id='1'";
-
 $result = mysql_query($sql);
 
 if ($pro=mysql_num_rows($result) ==0) 
@@ -42,51 +41,40 @@ if ($pro=mysql_num_rows($result) ==0)
 	
     while($row=mysql_fetch_assoc($result)) {
 			
-		$sql2="SELECT id, node_type_id FROM `nodes_hierarchy` where parent_id='".$row['id']."' and node_type_id='5' order by id DESC LIMIT 1 ";
+	$sql2="SELECT id, node_type_id FROM `nodes_hierarchy` where parent_id='".$row['id']."' and node_type_id='5' order by id DESC LIMIT 1 ";
         $result2 = mysql_query($sql2);
-		$row2=mysql_fetch_assoc($result2);
-        $ss=($row2['id']);
-		$sql7="SELECT  tcversion_id,testplan_id  FROM `testplan_tcversions` where testplan_id='".$row2['id']."' ";
-		$result7 = mysql_query($sql7);
-        //$report = "";	
-		//$report1 = "";
+	$row2=mysql_fetch_assoc($result2);
+        //$ss=($row2['id']);
+	$sql7="SELECT  tcversion_id,testplan_id  FROM `testplan_tcversions` where testplan_id='".$row2['id']."' ";
+	$result7 = mysql_query($sql7);
         $report = array();
-		$report1 = array();		
-		while($row7=mysql_fetch_array($result7)) {
-	    $sql4="SELECT t.testplan_id,r.status,r.tcversion_id FROM (SELECT testplan_id ,execution_ts,tcversion_id,status
+	$report1 = array();		
+	while($row7=mysql_fetch_array($result7)) {
+        $sql4="SELECT t.testplan_id,r.status,r.tcversion_id FROM (SELECT testplan_id ,execution_ts,tcversion_id,status
         FROM `executions` where testplan_id='".$row2['id']."'and tcversion_id= '".$row7['tcversion_id']."' ORDER BY execution_ts DESC LIMIT 50 )r 
-		INNER JOIN executions t ON t.testplan_id='".$row2['id']."'GROUP BY r.tcversion_id";	
+	INNER JOIN executions t ON t.testplan_id='".$row2['id']."'GROUP BY r.tcversion_id";	
         $result4= mysql_query($sql4);
         $row4=mysql_fetch_assoc($result4);		
         $report[] =  $row4["testplan_id"];
-		$report1[] = $row4["status"];		
-		}	
+	$report1[] = $row4["status"];		
+	}	
        	   
-		$row5=$report;
+	        $row5=$report;
 		$row6=$report1;
 		$rw=sizeof($row5);
 		$tt=array();
 		$tt=array_combine($row6,$row5);
-		
-		//print_r($tt['b']);//echo "</br>";
-		//print_r($report);
-		
-		 if(isset($tt['p'])== $row2['id']){ 
-           //$countp=1; 
-		   $countp=1;
-           }else{$countp=0;}
-		   if(isset($tt['f']) == $row2['id']){ 
-           $countf=$countf+1; 
-           }else{$countf=0;}
-          
-		   if(isset($tt['b']) == $row2['id']){ 
-           $countb=$countb+1; 
-		   }else{$countb=0;}
+	 if(isset($tt['p'])== $row2['id']){ 
+          $countp=1;}else{$countp=0;}
+         if(isset($tt['f']) == $row2['id']){ 
+           $countf=$countf+1;}else{$countf=0;}
+         if(isset($tt['b']) == $row2['id']){ 
+           $countb=$countb+1; }else{$countb=0;}
 
-		$category['data'][]= $row['name'];
+	$category['data'][]= $row['name'];
         $series1['data'][] = $countb;
         $series2['data'][] = $countf; 
-		$series3['data'][] = $countp;
+	$series3['data'][] = $countp;
 	
 
 }
