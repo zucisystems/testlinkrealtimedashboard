141f8c9140a1cb1b16bfe65aa243dbccc5ce80b6
MALARVIZHI.NA <malarvizhi.n@zucisystems.com>
2016-09-20 10:41:51 +0530

    Update project_data1.php

--- project_data1.php
+++ project_data1.php
@@ -38,8 +38,15 @@ $countf= 0;
 $countp= 0;
 $countb= 0;
 
-$sql = "select name, id, node_type_id from nodes_hierarchy where node_type_id='1'";
+ $sql = "select name, id, node_type_id from nodes_hierarchy where node_type_id='1'";
 $result = mysql_query($sql);
+//mysql_query("SET NAMES 'utf8'");
+//mysql_query("SET CHARACTER SET 'utf8'");
+
+//print_r($row1);	
+
+
+
 if ($pro=mysql_num_rows($result) ==0) 
 {
 
@@ -52,71 +59,67 @@ if ($pro=mysql_num_rows($result) ==0)
         $sql0="SELECT id  FROM `nodes_hierarchy` where parent_id='".$row['id']."' and node_type_id='5' order by id DESC LIMIT 1";	
         $result0 = mysql_query($sql0);	
         $row0=mysql_fetch_assoc($result0);
-		
         $sql1="SELECT executions.testplan_id as id FROM nodes_hierarchy JOIN executions ON executions.testplan_id='".$row0['id']."'";
         $result1 = mysql_query($sql1);	
         $row1=mysql_fetch_assoc($result1);
-		
-		if(!empty($row1['id'])){
 			
-        $sql7="SELECT  tcversion_id,testplan_id  FROM `testplan_tcversions` where testplan_id='".$row1['id']."' ";
+	    /*$sql2="SELECT id, node_type_id FROM `nodes_hierarchy` where parent_id='".$row1['id']."' and node_type_id='5' order by id DESC LIMIT 1 ";
+        $result2 = mysql_query($sql2);
+		$row2=mysql_fetch_assoc($result2);
+        $ss=($row1['id']);*/
+		if(!empty($row1['id'])){
+        $sql7="SELECT t.id,t.execution_type,r.testplan_id,r.tcversion_id FROM (SELECT testplan_id ,tcversion_id
+	    FROM `testplan_tcversions` where testplan_id='".$row1['id']."' ORDER BY tcversion_id DESC LIMIT 50 )r INNER JOIN 
+		tcversions t ON t.id=r.tcversion_id and t.execution_type='2' GROUP BY r.tcversion_id  ";
 		$result7 = mysql_query($sql7);
 		
-		while($row7=mysql_fetch_array($result7)){
-		$sql17="select id as tcversion_id from tcversions where id ='".$row7['tcversion_id']."' and execution_type='2'";
-		$result17 = mysql_query($sql17);
-		$row17=mysql_fetch_array($result17);
-		
+        //$report = "";	
+		//$report1 = "";
         $report = array();
 		$report1 = array();		
-		
-		//print_r($row17['tcversion_id']);
+		while($row7=mysql_fetch_array($result7)){
+			
 	    $sql4="SELECT t.testplan_id,r.status,r.tcversion_id FROM (SELECT testplan_id ,execution_ts,tcversion_id,status
-        FROM `executions` where testplan_id='".$row1['id']."'and tcversion_id= '".$row17['tcversion_id']."' ORDER BY execution_ts DESC LIMIT 50 )r 
+        FROM `executions` where testplan_id='".$row1['id']."'and tcversion_id= '".$row7['tcversion_id']."' ORDER BY execution_ts DESC LIMIT 50 )r 
 		INNER JOIN executions t ON t.testplan_id='".$row1['id']."'GROUP BY r.tcversion_id";	
         $result4= mysql_query($sql4);
         $row4=mysql_fetch_assoc($result4);	
-		//print_r($row17["tcversion_id"]);
-        $report[]=  $row17["tcversion_id"];
+        $report[]=  $row4["testplan_id"];
 		$report1[] = $row4["status"];
-		$report11[]=$row4['testplan_id'];
-		//}
-		
 		$row5[]=implode(",",$report1);
-		$rw5[]=implode(",",$report11);
 		$row6[]=implode(",",$report);
-		$row9[]=$row17['tcversion_id'];
+		$row9[]=$row1['id'];
+		$tt[$row4["testplan_id"]][]=$row5;
+        
+		$jj=array_slice($tt, -1, 1, true);	
 		$row7=array_slice($row5, -1, 1, true);
 		$row8=array_slice($row6, -1, 1, true);
 		$row10=array_slice($row9, -1, 1, true);
-		$row11=array_slice($report11, -1, 1, true);
-		//print_r(array_combine($row10,$row7));
-		$res=array_combine($row11,$row7);
-		//print_r(array_combine($row8,$row7));
-		//print_r($row4['testplan_id']);
-	    if(isset($row4['testplan_id'])){
-			
-		$countp=preg_match_all('/' . preg_quote('p', '/') . '/', $res[$row4['testplan_id']]);
+		$res=array_combine($row10,$row7);
+		
+		if(isset($res[$row1['id']])){
+		$countp=preg_match_all('/' . preg_quote('p', '/') . '/', $res[$row1['id']]);
            
-		$countf=preg_match_all('/' . preg_quote('f', '/') . '/', $res[$row4['testplan_id']]);
+		$countf=preg_match_all('/' . preg_quote('f', '/') . '/', $res[$row1['id']]);
        
-		$countb=preg_match_all('/' . preg_quote('b', '/') . '/', $res[$row4['testplan_id']]);
-	    $category['data'][]= $row['name'];
-		$series1['data'][] = $countb;
+		$countb=preg_match_all('/' . preg_quote('b', '/') . '/', $res[$row1['id']]);	
+	
+        //echo $row4["testplan_id"]->$row4["status"];echo"\n";	
+		}
+		$category['data'][]= $row['name'];
+        $series1['data'][] = $countb;
         $series2['data'][] = $countf; 
 		$series3['data'][] = $countp;
+           
 		} 
-		
-		}
-		
-       
-		}
+  
+		   
 		
 	
 		}
 }
 
-
+}
 $result = array();
 array_push($result,$category);
 array_push($result,$series1);
